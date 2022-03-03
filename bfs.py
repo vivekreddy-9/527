ecbceaa11fe2bc41b0e0c0601e99818fa4daeb0a
vivekreddy-9 <100821324+vivekreddy-9@users.noreply.github.com>
2022-03-03 09:41:43 +0530

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+class Graph:
+    def __init__(self):
+        self.Graph=defaultdict(list)
+    def addEdge(self,u,v):
+         self.Graph[u].append(v)
+    def BFS(self,s):
+        visited=[False]*(len(self.Graph))
+        queue=[]
+        queue.append(s)
+        visited[s]=True
+        while queue:
+             s=queue.pop(0)
+             print(s,end=" ")
+             for i in self.Graph[s]:
+                 if  visited[i]==False:
+                     queue.append(i)
+                     visited[i]=True
+g=Graph()
+g.addEdge(0,1)
+g.addEdge(0,2)
+g.addEdge(1,2)
+g.addEdge(2,0)
+g.addEdge(2,3)
+g.addEdge(3,3)
+print("following is bfs traversal")
+g.BFS(2)
